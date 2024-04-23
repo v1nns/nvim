68d9007159dbdf32463655ff272a9af7a76e784c
Vinicius Longaray <vmouralongaray@gmail.com>
2024-04-23 08:36:08 -0300

    Filter error message from neoscroll to avoid showing as popup

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -64,6 +64,7 @@ return {
           { find = "; before #%d+" },
           { find = "%d fewer lines" },
           { find = "%d more lines" },
+          { find = "Direction cannot be zero" },
         },
       },
       opts = { skip = true },
